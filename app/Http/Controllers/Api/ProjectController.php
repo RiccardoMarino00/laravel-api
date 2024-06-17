96816ad43243034cf890e43e68bbd21826e746ad
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-06-17 16:45:23 +0200

    Added: column image in projects table

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,10 +12,19 @@ class ProjectController extends Controller
     public function index(Request $request){
 
         // $projects = Project::all();
-        $results = Project::with('type', 'technologies')->paginate(10);
+        $results = Project::with('type', 'technologies')->paginate(6);
 
         return response()->json([
             'results' => $results
+        ]);
+    }
+
+    public function show(Project $project) {
+
+        $project->load('technologies', 'type');
+
+        return response()->json([
+            'project' => $project
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ use App\Http\Controllers\Api\ProjectController;
 // });
 
 Route::get('/projects', [ProjectController::class, 'index']);
+
+Route::get('/projects/{project:slug}', [ProjectController::class, 'show']);
